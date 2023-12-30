ec5c1fde2e2b33867e80f7963da3ed1793aea28a
sorki <srk@48.io>
2023-12-30 12:28:17 +0100

    Drop blockfrost_try

--- src/blockfrost_try.hrl
+++
@@ -1,9 +0,0 @@
--record(testr, {
-  str :: string(),
-  may :: string() | undefined,
-  wtf :: pls,
-  uni :: integer() | string(),
-  tup :: {integer(), string()}
-}).
-
--type testr() :: #testr{}.
